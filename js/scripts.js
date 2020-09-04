e02ddaa582dd2e6164d613d857500988fe44b462
EugeneDWK <60522215+eugenedwk@users.noreply.github.com>
2020-09-04 11:44:07 -0400

    New Pictures, About us, Reset Counter

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,24 @@ const count = document.getElementById('count');
 updateVisitCount();
 
 function updateVisitCount(){
-  fetch('https://api.countapi.xyz/update/yeokshi.life/97764835-bccc-415d-98d1-b765061afde7/?amount=1')
+  fetch('https://api.countapi.xyz/update/yeokshi.life/8013bede-d013-4b0a-abfc-7dad2f143637/?amount=1')
     .then(res => res.json())
     .then(res => {
       count.innerHTML = res.value
   });
-}
+};
+
+var quotes = [
+  'When life is hard but you still show up',
+  'Of course. Even when you\'re not sure.',
+  'Everyone else might be acting weird but you\'re still in it',
+  'Who else could be this awesome?',
+  'As expected',
+];
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+};
+
+newQuote();
