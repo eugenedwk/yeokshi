06dce1c22669e16b67d612347746682a2206982e
EugeneDWK <60522215+eugenedwk@users.noreply.github.com>
2022-01-15 23:35:48 -0500

    Add more stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+
+import { styled } from '@mui/system';
+
+const Hero = styled('div')({
+  backgroundColor: '#2b2b2b',
+  height: '100vh',
+  width: '100vw',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Typography fontSize="12rem" fontFamily="Dongle" >
+        <Hero>
+          <Box sx={{ 
+            fontSize: '12rem',
+            fontFamily: 'Dongle',
+            color: 'white'
+                      }}>
         역시
-        </Typography>
+          </Box>
+        </Hero>
       </header>
     </div>
   );
