9fb7630f05cd9a894c8bb786bf02e0b0b3a03cb9
EugeneDWK <60522215+eugenedwk@users.noreply.github.com>
2022-01-15 22:45:26 -0500

    Add font

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
+        <Typography fontSize="12rem" fontFamily="Dongle" >
         역시
-        </h1>
+        </Typography>
       </header>
     </div>
   );
